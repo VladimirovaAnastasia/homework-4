c94633c3ba6952c84734cb741c6d41bcfe867916
KRASAVA <vladimirova.a-a@yandex.ru>
2021-10-15 00:18:52 +0400

    Init

---
+++ solution/index.js
@@ -0,0 +1,33 @@
+module.exports = function (Homework) {
+
+    function promisify(fn) {
+        return function (...args) {
+            return new Promise((resolve) => {
+                args.push((result) => resolve(result));
+                fn.call(this, ...args);
+            });
+        };
+    }
+
+    return async (array, fn, initialValue, cb) => {
+        const fnAsync = promisify(fn);
+
+        const getArraySizeAsync = promisify(array.length);
+        const getArrayElAsync = promisify(array.get);
+        const addArrayElAsync = promisify(Homework.add);
+        const isLessArrayElAsync = promisify(Homework.less);
+
+        const arrayLength = await getArraySizeAsync();
+
+        let i = 0;
+        let acc = initialValue;
+
+        while (await isLessArrayElAsync(i, arrayLength)) {
+            let el = await getArrayElAsync(i);
+            acc = await fnAsync(acc, el, i, array);
+            i = await addArrayElAsync(i, 1);
+        }
+
+        return cb(acc);
+    }
+};
